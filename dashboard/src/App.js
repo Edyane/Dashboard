be6a703612fce98422758e85eef3d809ff2d6cc0
Edyane <edyanealves@gmail.com>
2023-01-30 22:31:07 -0300

    Adicionando 'TooltipComponent' ao projeto

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -15,7 +15,8 @@ const App = () => {
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000'}}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
+              <button type="button" className="text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray
+               text-white" style={{background:'blue', borderRadius:'50%'}}>
                 <FiSettings />
               </button>
             </TooltipComponent>
